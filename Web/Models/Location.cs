a11483469cd6a4400533cc696d3f6d3bfabf67e5
SonarBeserk <sonarbeserk@gmail.com>
2024-04-22 01:37:55 -0400

    Create model to represent a location

---
+++ Web/Models/Location.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models;
+
+public class Location(string name, string description)
+{
+    [Required]
+    public Guid Id { get; set; } = new();
+
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; } = name;
+
+    [Required]
+    [StringLength(200)]
+    public string Description { get; set; } = description;
+}
