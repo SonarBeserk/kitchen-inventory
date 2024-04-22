a4f14cf3a5644a88cec92559505a7e589fd8e504
SonarBeserk <sonarbeserk@gmail.com>
2024-04-22 01:32:36 -0400

    Create model to represent a product

---
+++ Web/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models;
+
+public class Product(string brand, string name, DateTime? expiry, ExpiryType? expiryType, bool perishable)
+{
+    [Required]
+    public Guid Id { get; set; } = new();
+
+    [Required]
+    [StringLength(100)]
+    public string Brand { get; set; } = brand;
+
+    [Required]
+    [StringLength(100)]
+    public string Name { get; set; } = name;
+
+    public DateTime? Expiry { get; set; } = expiry;
+    public ExpiryType? ExpiryType { get; set; } = expiryType;
+
+    public bool Perishable { get; set; } = perishable;
+}
+
+public enum ExpiryType
+{
+    Expiration = 1,
+    BestBy = 2
+}
