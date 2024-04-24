f39ba234c7417606a48a8ecf0e43a4031f433d3b
SonarBeserk <sonarbeserk@gmail.com>
2024-04-23 22:19:45 -0400

    Create initial version of the product service

---
+++ Web/Services/ProductService.cs
@@ -0,0 +1,41 @@
+﻿using Web.Models;
+
+namespace Web.Services;
+
+/// <summary>
+/// Represents the interface for the service that manages products
+/// </summary>
+public interface IProductService
+{
+    /// <summary>
+    /// Lists all tracked products
+    /// </summary>
+    /// <returns>All products being stored</returns>
+    public List<ProductModel> ListProducts();
+
+    /// <summary>
+    /// Adds a new product to store
+    /// </summary>
+    /// <param name="product">The new product</param>
+    public void AddProduct(ProductModel product);
+}
+
+public class ProductService : IProductService
+{
+    /// <summary>
+    /// Lists all tracked products
+    /// </summary>
+    /// <returns>All products being stored</returns>
+    public List<ProductModel> ListProducts()
+    {
+        return new List<ProductModel>();
+    }
+
+    /// <summary>
+    /// Adds a new product to store
+    /// </summary>
+    /// <param name="product">The new product</param>
+    public void AddProduct(ProductModel product)
+    {
+    }
+}
