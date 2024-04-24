21556ce8d1e4f8dbbc31a6bea64da5d45bf117f2
SonarBeserk <sonarbeserk@gmail.com>
2024-04-24 01:07:44 -0400

    Create data migration to add products table

---
+++ Web/Sql/0.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE IF NOT EXISTS products (
+    product_id TEXT PRIMARY KEY NOT NULL,
+    brand TEXT NOT NULL,
+    name TEXT NOT NULL,
+    expiry NUMERIC,
+    expiry_type INTEGER,
+    perishable BOOLEAN NOT NULL
+)
