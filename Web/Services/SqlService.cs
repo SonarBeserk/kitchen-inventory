765d509d1bdcda2c24398d610ae1ea326fe78fc8
SonarBeserk <sonarbeserk@gmail.com>
2024-04-24 02:10:47 -0400

    Break out the version check in sql service, Switch to user_version

--- Web/Services/SqlService.cs
+++ Web/Services/SqlService.cs
@@ -12,20 +12,19 @@ public static class SqlService
         }
         var conn = new SqliteConnection(connectionString);
         conn.Open(); // Open long-running connection to save memory and io calls
-        var command = conn.CreateCommand();
-        command.CommandText = @"PRAGMA schema_version;";
 
-        var userVersion = "";
-        using (var reader = command.ExecuteReader())
-        {
-            reader.Read();
-            userVersion = reader.GetString(0);
-;           if (string.IsNullOrWhiteSpace(userVersion))
-            {
-                throw new InvalidOperationException("Unable to find database version");
-            }
-        }
-
+        var userVersion = GetUserVersion(conn);
         Console.WriteLine($"Database Version: {userVersion}");
+    }
+
+    private static int GetUserVersion(SqliteConnection conn)
+    {
+        var command = conn.CreateCommand();
+        command.CommandText = @"PRAGMA user_version;";
+
+        using var reader = command.ExecuteReader();
+        reader.Read();
+        var rawVersion = reader.GetString(0);
+        return int.Parse(rawVersion);
     }
 }
