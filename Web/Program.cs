1ffbb7c97b0cf0a002a429be8bbb1cc51a57176c
SonarBeserk <sonarbeserk@gmail.com>
2024-04-23 23:41:24 -0400

    Add connection strings and dependency injection for sqlite database

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,8 +1,21 @@
+using System.Data;
+using Microsoft.Data.Sqlite;
+using Web.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// Handle dependency injection
+builder.Services.AddSingleton<SqliteConnection>(options =>
+{
+    var sqlSetting = builder.Configuration.GetConnectionString("sqlite");
+    if (string.IsNullOrWhiteSpace(sqlSetting))
+    {
+        throw new InvalidOperationException("Database connection string is missing or invalid");
+    }
+    return new SqliteConnection(sqlSetting);
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
