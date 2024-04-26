c50e2648ce35c1e8028c3c61b0a31f720006e50e
SonarBeserk <sonarbeserk@gmail.com>
2024-04-26 01:36:28 -0400

    Add additional constructors for products to allow passing in more properties

--- Web/Models/ProductModel.cs
+++ Web/Models/ProductModel.cs
@@ -2,8 +2,31 @@
 
 namespace Web.Models;
 
-public class ProductModel(string brand, string name, DateTime? expiry, ExpiryType? expiryType, bool perishable)
+public class ProductModel(
+    string brand,
+    string name,
+    DateTime? expiry,
+    ExpiryType? expiryType,
+    bool perishable,
+    Guid locationId)
 {
+    public ProductModel(string brand, string name, DateTime? expiry, ExpiryType? expiryType, bool perishable, LocationModel location)
+        : this(brand, name, expiry, expiryType, perishable, location.Id)
+    {
+    }
+
+    public ProductModel(Guid id, string brand, string name, DateTime? expiry, ExpiryType? expiryType, bool perishable, LocationModel location)
+        : this(brand, name, expiry, expiryType, perishable, location)
+    {
+        Id = id;
+    }
+
+    public ProductModel(Guid id, string brand, string name, DateTime? expiry, ExpiryType? expiryType, bool perishable, Guid locationId)
+        : this(brand, name, expiry, expiryType, perishable, locationId)
+    {
+        Id = id;
+    }
+
     [Required]
     public Guid Id { get; set; } = new();
 
@@ -16,7 +39,7 @@ public class ProductModel(string brand, string name, DateTime? expiry, ExpiryTyp
     public string Name { get; set; } = name;
 
     [Required]
-    public Guid LocationId { get; set; }
+    public Guid LocationId { get; set; } = locationId;
 
     public DateTime? Expiry { get; set; } = expiry;
     public ExpiryType? ExpiryType { get; set; } = expiryType;
