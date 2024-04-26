0cce4a044a837d18a854bff9b218294750b16337
SonarBeserk <sonarbeserk@gmail.com>
2024-04-26 16:47:50 -0400

    Implement ListProducts method in ProductService

--- Web/Services/ProductService.cs
+++ Web/Services/ProductService.cs
@@ -1,4 +1,5 @@
-﻿using System.Data;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
 using Microsoft.Data.Sqlite;
 using Web.Models;
 
@@ -32,7 +33,27 @@ public class ProductService(SqliteConnection db) : IProductService
     /// <returns>All products being stored</returns>
     public List<ProductModel> ListProducts()
     {
-        return new List<ProductModel>();
+        var productsList = new List<ProductModel>();
+
+        var command = _db.CreateCommand();
+        command.CommandText = @"SELECT products.product_id, products.brand, products.name, products.expiry, products.expiry_type, products.perishable, products.location_id from products;";
+
+        using var reader = command.ExecuteReader();
+        while (reader.Read())
+        {
+            var product = new ProductModel(
+                (Guid)reader["product_id"],
+                (string)reader["brand"],
+                (string)reader["name"],
+                (DateTime?)reader["expiry"],
+                (ExpiryType?)reader["expiry_type"],
+                (bool)reader["perishable"],
+                (Guid)reader["location_id"]);
+
+            productsList.Add(product);
+        }
+
+        return productsList;
     }
 
     /// <summary>
