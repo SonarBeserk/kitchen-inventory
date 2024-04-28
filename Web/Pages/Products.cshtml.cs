1fcfe454ae65332a59a91b0e3724c2414864d75b
SonarBeserk <sonarbeserk@gmail.com>
2024-04-27 23:41:59 -0400

    Create products page to print current products

---
+++ Web/Pages/Products.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Web.Models;
+using Web.Services;
+
+namespace Web.Pages;
+
+public class ProductsModel : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+    private readonly IProductService _productService;
+
+    public ProductsModel(ILogger<PrivacyModel> logger, IProductService productService)
+    {
+        _logger = logger;
+        _productService = productService;
+    }
+
+    public void OnGet()
+    {
+        var products = _productService.ListProducts();
+        foreach (var productModel in products)
+        {
+            _logger.Log(LogLevel.Information, "Product: {brand} {name}", productModel.Brand, productModel.Name);
+        }
+    }
+}
