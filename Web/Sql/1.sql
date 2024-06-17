a18653dcf9cfa960ec7ac6efc7aeee0fa5fc1db6
SonarBeserk <sonarbeserk@gmail.com>
2024-06-17 19:33:02 -0400

    Reformat sql file

--- Web/Sql/1.sql
+++ Web/Sql/1.sql
@@ -1,23 +1,26 @@
-﻿CREATE TABLE IF NOT EXISTS products (
+﻿CREATE TABLE IF NOT EXISTS products
+(
     product_id TEXT PRIMARY KEY NOT NULL,
-    brand TEXT NOT NULL,
-    name TEXT NOT NULL
+    brand      TEXT             NOT NULL,
+    name       TEXT             NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS locations (
+CREATE TABLE IF NOT EXISTS locations
+(
     location_id TEXT PRIMARY KEY NOT NULL,
-    name TEXT NOT NULL,
+    name        TEXT             NOT NULL,
     description TEXT
 );
 
-CREATE TABLE IF NOT EXISTS inventory(
+CREATE TABLE IF NOT EXISTS inventory
+(
     inventory_id TEXT PRIMARY KEY NOT NULL,
-    product_id TEXT,
-    expiry NUMERIC,
-    expiry_type INTEGER,
-    perishable BOOLEAN NOT NULL,
-    amount NUMERIC,
-    location_id TEXT,
-    FOREIGN KEY (product_id) REFERENCES products(product_id),
-    FOREIGN KEY (location_id) REFERENCES locations(location_id)
+    product_id   TEXT,
+    expiry       NUMERIC,
+    expiry_type  INTEGER,
+    perishable   BOOLEAN          NOT NULL,
+    amount       NUMERIC,
+    location_id  TEXT,
+    FOREIGN KEY (product_id) REFERENCES products (product_id),
+    FOREIGN KEY (location_id) REFERENCES locations (location_id)
 );
